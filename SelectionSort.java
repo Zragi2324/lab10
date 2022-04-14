b113ddd9692b4a040a4b1e8daf20327df7004541
Alzraji <alialzragi2324@gmail.com>
2022-04-14 15:08:57 -0400

    fixed comments

--- SelectionSort.java
+++ SelectionSort.java
@@ -10,13 +10,7 @@ package Testing;
 
 
 
-		    /* A simple SelectionSort algorithm
-		     * pre-condition:
-		     * post-condition:
-		     * inputs:
-		     * outputs:
-		     * special conditions:
-		     */
+		   
 		    public int[] basicSelectionSort(int[] x) {
 		        for (int i = 0; i < x.length; ++i) {
 		            for (int j= i+1; j < x.length; ++j) {
